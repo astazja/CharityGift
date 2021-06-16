71a51c1aaed1d5ac7b4c3c85d91d86c0ddcea93a
Katarzyna Pacewicz <katarzyna@pacewi.cz>
2021-06-16 14:17:47 +0200

    list of foundation

--- src/main/java/pl/coderslab/charity/service/implementation/ImplInstitutionService.java
+++ src/main/java/pl/coderslab/charity/service/implementation/ImplInstitutionService.java
@@ -1,8 +1,11 @@
 package pl.coderslab.charity.service.implementation;
 
 import org.springframework.stereotype.Service;
+import pl.coderslab.charity.model.Institution;
 import pl.coderslab.charity.repository.InstitutionRepository;
 import pl.coderslab.charity.service.InstitutionService;
+
+import java.util.List;
 
 @Service
 public class ImplInstitutionService implements InstitutionService {
@@ -11,5 +14,10 @@ public class ImplInstitutionService implements InstitutionService {
 
     public ImplInstitutionService(InstitutionRepository institutionRepository) {
         this.institutionRepository = institutionRepository;
+    }
+
+    @Override
+    public List<Institution> allInstitutions() {
+        return institutionRepository.findAll();
     }
 }
